21e3bfad219c0d207cd75638b1dbcd1cb3c98bb8
Seongju Park <psj066@naver.com>
2026-03-05 10:37:35 +0900

    changed setting and made deploy workflows

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: 'https://psj066.github.io',
+  base: '/MyCV',
 });
