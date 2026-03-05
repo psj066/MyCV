db10e47e4209f84f410f014385d27e3eb6c4e31d
Seongju Park <psj066@naver.com>
2026-03-05 10:41:15 +0900

    Changed astroconfigfile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://psj066.github.io',
-  base: '/MyCV',
+  site: 'https://seongjupark.com',
+  base: '/',
 });
